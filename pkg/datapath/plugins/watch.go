9cefddc01571d279d36209af910a8215bc845fd6
Jordan Rife <jordan@jrife.io>
2026-02-06 11:26:46 -0800

    Regenerate endpoints when plugins change

--- pkg/datapath/plugins/watch.go
+++ pkg/datapath/plugins/watch.go
@@ -7,12 +7,15 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cilium/cilium/pkg/endpoint/regeneration"
+	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	"github.com/cilium/statedb"
 )
 
-func registerDPPWatcher(jg job.Group, db *statedb.DB, table statedb.Table[DatapathPlugin]) {
+func registerDPPWatcher(jg job.Group, db *statedb.DB, table statedb.Table[DatapathPlugin], endpointManager endpointmanager.EndpointManager) {
+
 	jg.Add(job.OneShot(
 		"follow",
 		func(ctx context.Context, _ cell.Health) error {
@@ -33,6 +36,14 @@ func registerDPPWatcher(jg job.Group, db *statedb.DB, table statedb.Table[Datapa
 					fmt.Printf("Name: %s, AttachmentPolicy: %s (revision: %d, deleted: %v)\n",
 						e.Name, e.AttachmentPolicy, rev, change.Deleted)
 				}
+
+				regenRequest := &regeneration.ExternalRegenerationMetadata{
+					Reason:            "Datapath plugins updated",
+					RegenerationLevel: regeneration.RegenerateWithoutDatapath,
+					ParentContext:     ctx,
+				}
+				endpointManager.RegenerateAllEndpoints(regenRequest).Wait()
+
 				// Wait until there's new changes to consume.
 				select {
 				case <-ctx.Done():
