75331779d5290b286929cb9357ed5ba69b29101d
Jordan Rife <jrife@google.com>
2026-03-12 12:09:59 -0700

    plugins, bpf: Implement BPF collection instrumentation logic
    Signed-off-by: Jordan Rife <jrife@google.com>

--- pkg/datapath/loader/plugins.go
+++ pkg/datapath/loader/plugins.go
@@ -1,8 +1,309 @@
 package loader
 
 import (
+	"errors"
+	"fmt"
 	"sort"
+
+	"github.com/cilium/cilium/api/v1/datapathplugins"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/asm"
+	"github.com/cilium/ebpf/btf"
 )
+
+func preHookSubprogName(pluginName string) string {
+	return fmt.Sprintf("__pre_hook_%s__", pluginName)
+}
+
+func postHookSubprogName(pluginName string) string {
+	return fmt.Sprintf("__post_hook_%s__", pluginName)
+}
+
+// hooksSpec tracks inter-plugin dependencies and applies them to instrument
+// programs in BPF collections with appropriate dispatchers.
+type hooksSpec struct {
+	hooks map[string]map[datapathplugins.HookType]*pluginDependencyGraph
+}
+
+func newHooksSpec() *hooksSpec {
+	return &hooksSpec{
+		hooks: make(map[string]map[datapathplugins.HookType]*pluginDependencyGraph),
+	}
+}
+
+// hook returns the plugin dependency graph for the hook point indicated by
+// (target, hookType). Consumers can then add constraints or plugins to this
+// dependency graph.
+func (hs *hooksSpec) hook(target string, hookType datapathplugins.HookType) *pluginDependencyGraph {
+	if hs.hooks[target] == nil {
+		hs.hooks[target] = map[datapathplugins.HookType]*pluginDependencyGraph{
+			datapathplugins.HookType_PRE:  &pluginDependencyGraph{},
+			datapathplugins.HookType_POST: &pluginDependencyGraph{},
+		}
+	}
+
+	return hs.hooks[target][hookType]
+}
+
+// instrumentCollection prepares an InstrumentCollectionRequest for each plugin
+// that requested hooks and generates a program patch for each program that
+// requires instrumentation. It doesn't patch program instructions directly.
+// Patching is instead deferred until after reachability analysis and pruning
+// happen.
+func (hs *hooksSpec) instrumentCollection(cs *ebpf.CollectionSpec) (map[string]*datapathplugins.InstrumentCollectionRequest, map[string]func(asm.Instructions) (asm.Instructions, error), error) {
+	var err error
+
+	hooks := make(map[string]*datapathplugins.InstrumentCollectionRequest)
+	programPatches := make(map[string]func(asm.Instructions) (asm.Instructions, error))
+
+	for hookTarget, hookTypes := range hs.hooks {
+		pre, sortErr := hookTypes[datapathplugins.HookType_PRE].sort()
+		if sortErr != nil {
+			err = errors.Join(err, fmt.Errorf("%s/%s: %w", hookTarget, datapathplugins.HookType_PRE, sortErr))
+			continue
+		}
+		post, sortErr := hookTypes[datapathplugins.HookType_POST].sort()
+		if sortErr != nil {
+			err = errors.Join(err, fmt.Errorf("%s/%s: %w", hookTarget, datapathplugins.HookType_POST, sortErr))
+			continue
+		}
+		patch, err := hs.instrumentProgram(cs.Programs[hookTarget], pre, post, hooks)
+		if err != nil {
+			err = errors.Join(err, fmt.Errorf("instrumenting %s: %w", hookTarget, err))
+
+			continue
+		}
+		programPatches[hookTarget] = patch
+	}
+
+	return hooks, programPatches, err
+}
+
+// instrumentProgram generates a program patcher that prepends a dispatcher that
+// invokes pre-program hooks, then invokes the original program, and finally
+// invokes post-program hooks. Something like this:
+//
+//	int dispatch(void *ctx) {
+//	    int orig_ret, ret;
+//
+//	    ret = __pre_hook_plugin_a__(ctx);
+//	    if (ret != RET_PROCEED)
+//	        return ret;
+//	    ret = __pre_hook_plugin_b__(ctx);
+//	    if (ret != RET_PROCEED)
+//	        return ret;
+//	    ...
+//	    orig_ret = original_cilium_prog(ctx);
+//	    ...
+//	    ret = __post_hook_plugin_a__(ctx, orig_ret);
+//	    if (ret != RET_PROCEED)
+//	        return ret;
+//	    ret = __post_hook_plugin_b__(ctx, orig_ret);
+//	    if (ret != RET_PROCEED)
+//	        return ret;
+//
+//	    return orig_ret;
+//	}
+//
+//	int original_cilium_prog(void *ctx) {
+//	    ...
+//	}
+//
+//	int __pre_hook_plugin_a__(void *ctx) {
+//	    volatile int ret = RET_PROCEED;
+//	    return ret;
+//	}
+//
+//	int __pre_hook_plugin_b__(void *ctx) {
+//	    volatile int ret = RET_PROCEED;
+//	    return ret;
+//	}
+//
+//	int __post_hook_plugin_a__(void *ctx, int orig_ret) {
+//	    volatile int ret = RET_PROCEED;
+//	    return ret;
+//	}
+//
+//	int __post_hook_plugin_a__(void *ctx, int orig_ret) {
+//	    volatile int ret = RET_PROCEED;
+//	    return ret;
+//	}
+func (hs *hooksSpec) instrumentProgram(ps *ebpf.ProgramSpec, pre []string, post []string, hooks map[string]*datapathplugins.InstrumentCollectionRequest) (func(asm.Instructions) (asm.Instructions, error), error) {
+	btfMeta := btf.FuncMetadata(&ps.Instructions[0])
+	funcProto, hasFuncProto := btfMeta.Type.(*btf.FuncProto)
+	if !hasFuncProto {
+		return nil, fmt.Errorf("unable to extract function BTF info for target program")
+	}
+
+	var prologue asm.Instructions
+
+	// Preserve ctx in R6, callee saved register.
+	prologue = append(prologue, asm.Mov.Reg(asm.R6, asm.R1))
+
+	for _, plugin := range pre {
+		// ret = __pre_hook_xxx__(ctx);
+		// if (ret != RET_VAL_PROCEED)
+		//     return ret;
+		subprogName := preHookSubprogName(plugin)
+		prologue = append(prologue,
+			asm.Mov.Reg(asm.R1, asm.R6),
+			asm.Call.Label(subprogName),
+			asm.JNE.Imm32(asm.R0, retValProceed(ps), "return"),
+		)
+		if hooks[plugin] == nil {
+			hooks[plugin] = &datapathplugins.InstrumentCollectionRequest{}
+		}
+		hooks[plugin].Hooks = append(hooks[plugin].Hooks, &datapathplugins.InstrumentCollectionRequest_Hook{
+			AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+				SubprogName: subprogName,
+			},
+			Type:   datapathplugins.HookType_PRE,
+			Target: ps.Name,
+		})
+	}
+
+	// orig_ret = original_cilium_prog(ctx);
+	prologue = append(prologue,
+		asm.Mov.Reg(asm.R1, asm.R6),
+		asm.Call.Label(btfMeta.Name),
+		asm.Mov.Reg(asm.R7, asm.R0),
+	)
+
+	for _, plugin := range post {
+		// ret = __post_hook_xxx__(ctx, orig_ret);
+		// if (ret != RET_VAL_PROCEED)
+		//     return ret;
+		subprogName := postHookSubprogName(plugin)
+		prologue = append(prologue,
+			asm.Mov.Reg(asm.R1, asm.R6),
+			asm.Mov.Reg(asm.R2, asm.R7),
+			asm.Call.Label(subprogName),
+			asm.JNE.Imm32(asm.R0, retValProceed(ps), "return"),
+		)
+		if hooks[plugin] == nil {
+			hooks[plugin] = &datapathplugins.InstrumentCollectionRequest{}
+		}
+		hooks[plugin].Hooks = append(hooks[plugin].Hooks, &datapathplugins.InstrumentCollectionRequest_Hook{
+			AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+				SubprogName: subprogName,
+			},
+			Type:   datapathplugins.HookType_POST,
+			Target: ps.Name,
+		})
+	}
+
+	prologue = append(prologue, asm.Mov.Reg(asm.R0, asm.R7))
+	prologue = append(prologue, clampAndReturn(ps, "return")...)
+
+	entryName := fmt.Sprintf("__%s__", btfMeta.Name)
+	prologue[0] = btf.WithFuncMetadata(
+		prologue[0].
+			WithSymbol(entryName).
+			WithSource(asm.Comment(entryName)),
+		&btf.Func{
+			Name:    entryName,
+			Type:    funcProto,
+			Linkage: btf.GlobalFunc,
+			Tags:    btfMeta.Tags,
+		})
+
+	var epilogue asm.Instructions
+
+	postHookProto := *funcProto
+	postHookProto.Params = append(
+		append([]btf.FuncParam(nil), postHookProto.Params...),
+		btf.FuncParam{Name: "ret", Type: funcProto.Return},
+	)
+
+	for _, plugin := range pre {
+		hookName := preHookSubprogName(plugin)
+		epilogue = append(epilogue,
+			btf.WithFuncMetadata(
+				asm.Mov.Imm(asm.R0, retValProceed(ps)).
+					WithSymbol(hookName).
+					WithSource(asm.Comment(hookName)),
+				&btf.Func{
+					Name: hookName,
+					Type: funcProto,
+					// BTF_FUNC_GLOBAL ensures programs are independently verified.
+					Linkage: btf.GlobalFunc,
+				}),
+			asm.Return(),
+		)
+	}
+	for _, plugin := range post {
+		hookName := postHookSubprogName(plugin)
+		epilogue = append(epilogue,
+			btf.WithFuncMetadata(
+				asm.Mov.Imm(asm.R0, retValProceed(ps)).
+					WithSymbol(hookName).
+					WithSource(asm.Comment(hookName)),
+				&btf.Func{
+					Name: hookName,
+					Type: &postHookProto,
+					// BTF_FUNC_GLOBAL ensures programs are independently verified.
+					Linkage: btf.GlobalFunc,
+				}),
+			asm.Return(),
+		)
+	}
+
+	return func(insns asm.Instructions) (asm.Instructions, error) {
+		return append(prologue, append(insns, epilogue...)...), nil
+	}, nil
+}
+
+func retValProceed(ps *ebpf.ProgramSpec) int32 {
+	switch ps.AttachType {
+	case ebpf.AttachCGroupInet4Bind, ebpf.AttachCGroupInet6Bind,
+		ebpf.AttachCGroupUDP4Recvmsg, ebpf.AttachCGroupUDP6Recvmsg,
+		ebpf.AttachCgroupInet4GetPeername, ebpf.AttachCgroupInet6GetPeername,
+		ebpf.AttachCgroupInet4GetSockname, ebpf.AttachCgroupInet6GetSockname,
+		ebpf.AttachCGroupInet4Connect, ebpf.AttachCGroupInet6Connect,
+		ebpf.AttachCGroupInet4PostBind, ebpf.AttachCGroupInet6PostBind,
+		ebpf.AttachCGroupUDP4Sendmsg, ebpf.AttachCGroupUDP6Sendmsg,
+		ebpf.AttachCgroupInetSockRelease:
+		return 1 // SYS_PROCEED
+	default:
+		return -1 // TCX_NEXT / TC_ACT_UNSPEC
+	}
+
+}
+
+// clamAndReturn makes sure the verifier's return value range check is satisfied
+// for certain attach types before exiting.
+//
+// https://github.com/torvalds/linux/blob/8a30aeb0d1b4e4aaf7f7bae72f20f2ae75385ccb/kernel/bpf/verifier.c#L17901
+func clampAndReturn(ps *ebpf.ProgramSpec, label string) asm.Instructions {
+	var defaultVal int64
+	var min, max int32
+
+	switch ps.AttachType {
+	case ebpf.AttachCGroupInet4Bind, ebpf.AttachCGroupInet6Bind:
+		min, max, defaultVal = 0, 3, 0
+	case ebpf.AttachCGroupUDP4Recvmsg, ebpf.AttachCGroupUDP6Recvmsg,
+		ebpf.AttachCgroupInet4GetPeername, ebpf.AttachCgroupInet6GetPeername,
+		ebpf.AttachCgroupInet4GetSockname, ebpf.AttachCgroupInet6GetSockname:
+		min, max, defaultVal = 1, 1, 1
+	case ebpf.AttachCGroupInet4Connect, ebpf.AttachCGroupInet6Connect,
+		ebpf.AttachCGroupInet4PostBind, ebpf.AttachCGroupInet6PostBind,
+		ebpf.AttachCGroupUDP4Sendmsg, ebpf.AttachCGroupUDP6Sendmsg,
+		ebpf.AttachCgroupInetSockRelease:
+		min, max, defaultVal = 0, 1, 0
+	default:
+		return []asm.Instruction{
+			asm.Return().WithSymbol(label),
+		}
+	}
+
+	return []asm.Instruction{
+		asm.JGT.Imm(asm.R0, max, "set_default").WithSymbol(label),
+		asm.JLT.Imm(asm.R0, min, "set_default"),
+		asm.Ja.Label("exit"),
+		asm.LoadImm(asm.R0, defaultVal, asm.DWord).WithSymbol("set_default"),
+		asm.Return().WithSymbol("exit"),
+	}
+}
 
 type node struct {
 	exists        bool
